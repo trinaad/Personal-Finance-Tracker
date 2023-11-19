f38c70e57b4a93cdac99e6cd0e1226883735579d
Tri_007 <59909109+trinaad@users.noreply.github.com>
2023-11-19 12:20:39 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.bgCyan.white);
+    return conn; // Return the connection object if needed elsewhere in your code
   } catch (error) {
-    console.log(`${error}`.bgRed);
+    console.error(`${error}`.bgRed);
+    process.exit(1);
   }
 };
 
