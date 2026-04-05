c38bf11144e81a87d5396573556b221371192af7
Tri_007 <59909109+trinaad@users.noreply.github.com>
2026-04-05 17:01:53 +0530

    Fix static file path for client build

--- server/server.js
+++ server/server.js
@@ -26,10 +26,9 @@ app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
 //static files
-app.use(express.static(path.join(__dirname, "./client/build")));
-
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 //port
